6ef6e98ee422ded3cb022dd3e477efbbc6dd3b79
JosieAilsa <josie.a.newman@gmail.com>
2021-08-15 18:32:37 +0100

    pseudo code

---
+++ README.txt
@@ -0,0 +1,1 @@
+My first javascript game, built using only vanilla js, SCSS/CSS and HTML5.
---
+++ script.js
@@ -0,0 +1,36 @@
+/*1. When user clicks start get rid of the start div
+
+//2. Create new apple in a random location
+
+//3.Create the static snake
+    // -appendChild to main board container
+    //- Set its size based on row/col start/end 
+    //Give it a background colour - 
+    //For the head have a seperate class with an svg bg image
+
+4. Set up the logic of how the snake will move
+        Every second .25 a second if a button is pressed the the snake will move:
+                 If the previous key was up/down :
+                    Can move +1 or -1 x
+                 If the previous key was left or right:
+                    Can move +1 or -1 y 
+    
+                        
+Each iteration + or - 1 to x coordinate if the l or r arrow is pressed 
+Each iteration + or - 1 to y coordinate if the up or down arrow is pressed 
+
+    If the snake hits x or y coordinate of 1 or 25 = game over
+    if snakeHead === x & y of other snake body also game over 
+
+    if snakeHead === x&y of apple = points aded to scores string 
+
+5. Create the snake with the logic from above
+    //To make snake move change the grid area of each part 
+    
+    Add score string to the innerhtml 
+
+
+
+
+
+*/
