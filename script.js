aa3fad7643465eda77758c3d76fd36e7b0c6806e
JosieAilsa <josie.a.newman@gmail.com>
2021-08-16 10:40:27 +0100

    pseudo code

--- script.js
+++ script.js
@@ -1,33 +1,41 @@
-/*1. When user clicks start get rid of the start div
+/*
+1. When user clicks start hide the start div
 
-//2. Create new apple in a random location
+2. Create new apple in a random location
+- Create a div in the random location using appendChild 
+- Style with CSS classes using svg as bg
+- Use grid col/row start to generate random values (using mathRandom) then +1 to each number to get apple location
 
-//3.Create the static snake
-    // -appendChild to main board container
-    //- Set its size based on row/col start/end 
-    //Give it a background colour - 
-    //For the head have a seperate class with an svg bg image
+3.Create the static snake
+- Set size as 1 col/row width
+- Add stying 
+- For the head have a seperate class with an svg bg image
 
-4. Set up the logic of how the snake will move
-        Every second .25 a second if a button is pressed the the snake will move:
-                 If the previous key was up/down :
-                    Can move +1 or -1 x
-                 If the previous key was left or right:
-                    Can move +1 or -1 y 
-    
-                        
-Each iteration + or - 1 to x coordinate if the l or r arrow is pressed 
-Each iteration + or - 1 to y coordinate if the up or down arrow is pressed 
+4. Logic of how the snake will move
+- Every second the snake will move based on last button pressed by user  
+    If the previous key was up/down :
+        Will move +1(right) or -1(left) x
+    If the previous key was left or right:
+        Can move +1(down) or -1(up) y 
+
+//Winnning the round//
+
+    if snakeHead === x&y of apple = +50 points added to score value
+
+
+//Ending the game//
 
     If the snake hits x or y coordinate of 1 or 25 = game over
     if snakeHead === x & y of other snake body also game over 
 
-    if snakeHead === x&y of apple = points aded to scores string 
+    Show play again loop
 
-5. Create the snake with the logic from above
-    //To make snake move change the grid area of each part 
-    
-    Add score string to the innerhtml 
+    Push score to high scores array. Use .filter to find IF high score and add to innnerHTML on game UI 
+
+    Show the play again div
+
+5. Create an array for high score and variable for current score. Each time snake eats apple score += points. 
+
 
 
 
